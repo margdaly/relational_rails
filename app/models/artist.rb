3fbf17e62e58f6cae27b61d44921a399b82a01a4
margdaly <margaret.daly@gmail.com>
2023-03-24 18:45:34 -0600

    test: Add test for #paint_count

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,4 +4,9 @@ class Artist < ApplicationRecord
   def self.order_by_most_recently_created
     order(created_at: :desc)
   end
+
+  def paint_count
+    # require 'pry'; binding.pry
+    self.paints.count
+  end
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,7 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Artist do
-  describe "relationships" do
-    it { should have_many :paints }
+  it { should have_many :paints }
+
+  describe '#instance_methods' do
+    let!(:coyote) { Artist.create!(name: 'Criminal Coyote', sponsored: false, rank: 9) }
+    let!(:mars) { Artist.create!(name: 'Farris Mars', sponsored: true, rank: 8) }
+    let!(:graves) { Artist.create!(name: 'Mike Graves', sponsored: true, rank: 7) }
+    let!(:toffee) { graves.paints.create!(name: 'Toffee', 
+                                          brand: 'Montana', 
+                                          high_pressure: false, 
+                                          opacity: 3) }
+    let!(:latte) { graves.paints.create!(name: 'Latte', 
+                                         brand: 'Montana', 
+                                         high_pressure: false, 
+                                         opacity: 4) }
+    let!(:hazelnut) { graves.paints.create!(name: 'Hazelnut', 
+                                            brand: 'Molotow', 
+                                            high_pressure: true, 
+                                            opacity: 5) }
+    
+    describe '#order_by_most_recently_created' do
+      it 'orders paints by most recently created' do
+        expect(graves.paints.last).to eq(hazelnut)
+      end
+    end
+
+    describe '#paint_count' do
+      it 'counts the number of paints' do
+        expect(graves.paint_count).to eq(3)
+      end
+    end
   end
 end
