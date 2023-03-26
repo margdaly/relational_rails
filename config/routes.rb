56f20fb781fa667691e61f38f81b9f37dd7b94bd
margdaly <margaret.daly@gmail.com>
2023-03-25 23:43:54 -0600

    Add get from art_index to art#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/artists', to: 'artists#index'
+  get '/artists/new', to: 'artists#new'
   get '/artists/:id', to: 'artists#show'
   get '/paints', to: 'paints#index'
   get '/paints/:id', to: 'paints#show'
