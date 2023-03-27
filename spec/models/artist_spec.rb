37c06ae6327d912946836b1844bc7d5fa093c116
margdaly <margaret.daly@gmail.com>
2023-03-26 20:50:47 -0600

    test: Add order_by_most_recent_created to artists spec

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -10,9 +10,9 @@ RSpec.describe Artist do
   end
 
   describe '#instance_methods' do
-    let!(:coyote) { Artist.create!(name: 'Criminal Coyote', sponsored: false, rank: 9) }
-    let!(:mars) { Artist.create!(name: 'Farris Mars', sponsored: true, rank: 8) }
-    let!(:graves) { Artist.create!(name: 'Mike Graves', sponsored: true, rank: 7) }
+    let!(:coyote) { Artist.create!(name: 'Criminal Coyote', sponsored: false, rank: 9, created_at: 'Sun, 26 Mar 2023 03:28:02 UTC +00:00') }
+    let!(:graves) { Artist.create!(name: 'Mike Graves', sponsored: true, rank: 8, created_at: 'Sat, 25 Mar 2023 03:28:02 UTC +00:00') }
+    let!(:mars) { Artist.create!(name: 'Farris Mars', sponsored: true, rank: 9, created_at: 'Fri, 24 Mar 2023 03:28:02 UTC +00:00') }
     let!(:toffee) { graves.paints.create!(name: 'Toffee', 
                                           brand: 'Montana', 
                                           high_pressure: false, 
@@ -26,6 +26,17 @@ RSpec.describe Artist do
                                             high_pressure: true, 
                                             opacity: 5) }
 
+    describe '#order_by_most_recently_created' do
+      it 'sorts Artists with most recently created first' do
+        expect(Artist.order_by_most_recently_created).to eq([coyote, graves, mars])
+
+        graves.created_at = 'Mon, 27 Mar 2023 03:28:02 UTC +00:00'
+        graves.save
+
+        expect(Artist.order_by_most_recently_created).to eq([graves, coyote, mars])
+      end
+    end
+                                          
     describe '#paint_count' do
       it 'counts the number of paints' do
         expect(graves.paint_count).to eq(3)
