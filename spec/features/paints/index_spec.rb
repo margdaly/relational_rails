7cb18e06fab55c3c41e084777875e9c32d715426
margdaly <margaret.daly@gmail.com>
2023-03-27 21:48:58 -0600

    test: Add delete link to artist and paint show page

--- spec/features/paints/index_spec.rb
+++ spec/features/paints/index_spec.rb
@@ -84,5 +84,14 @@ RSpec.describe Paint, type: :feature do
       expect(current_path).to eq("/paints/#{latte.id}/edit")
       expect(page).to have_content("Update #{latte.name} Form")
     end
+
+    it "has link to delete paint" do
+      visit "/paints"
+
+      click_on("Delete #{toffee.name}")
+
+      expect(current_path).to eq("/paints")
+      expect(page).to_not have_content("#{toffee.name}")
+    end
   end
 end
